7dbf7b3dad47858c001e89556a78e4aa4d551ac5
Vitaly Chemezov <vitaly.chemezov@gmail.com>
2019-08-26 23:42:29 +0200

    Add changes

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === "production") {
 } else {
   app.use(express.static(path.join(__dirname, "/public")));
   app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
   });
 }
 
